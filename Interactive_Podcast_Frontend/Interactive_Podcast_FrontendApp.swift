59e9bf38a8265ad5acb7216b6b50262f238d24cb
Yan Pan <yanpan.0508@gmail.com>
2024-09-02 23:13:11 -0700

    Initial Commit

---
+++ Interactive_Podcast_Frontend/Interactive_Podcast_FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_Podcast_FrontendApp.swift
+//  Interactive_Podcast_Frontend
+//
+//  Created by Yan Pan on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_Podcast_FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
